277e0c84acadf926384006aef8274d04c076534b
Michael Chan <mikechansas@gmail.com>
2022-02-10 17:35:49 -0800

    Implement Login Button Component and integrate into App.js.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import LoginButton from './components/login-button/login-button.component';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginButton />
       </header>
     </div>
   );
