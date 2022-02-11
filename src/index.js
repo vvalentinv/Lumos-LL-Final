e6a2b972bdc1187f7c176605a4618bc651bd60e0
Michael Chan <mikechansas@gmail.com>
2022-02-10 17:33:43 -0800

    Implement minor bug fix in index.js.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
-    clientID={clientID}
+    clientId={clientID}
     redirectUri={window.location.origin}>
     <App />
   </Auth0Provider>,
   document.getElementById('root')
 );
-
