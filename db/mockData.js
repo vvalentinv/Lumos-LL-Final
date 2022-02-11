8723850e42403ecac15b19e40ae632dab6fa1597
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-02-11 12:24:46 -0500

    mock data

---
+++ db/mockData.js
@@ -0,0 +1,167 @@
+arrayOfDecks = [
+  {
+    id: 1,
+    user_id: 5,
+    name: "React",
+    description: "React based flashcards set",
+    category_id: 2,
+  },
+  {
+    id: 3,
+    user_id: 2,
+    name: "Node",
+    description: "NodeJS based flashcards set",
+    category_id: 2,
+  },
+  {
+    id: 8,
+    user_id: 1,
+    name: "Postgresql",
+    description: "PostgreSQL based flashcards set",
+    category_id: 2,
+  }
+]
+
+arrayOfDecksWithCards = [
+  { '1': [1, 2, 3, 4, 5, 6, 7, 9, 10] },
+  { '3': [2, 4, 6, 8, 10, 11, 12] },
+  { '8': [2, 4, 6, 8, 10, 13, 14] }
+]
+
+arrayOfFlashcards = [
+  {
+    id: 1,
+    user_id: 1,
+    question: "Q1",
+    url: "/",
+    answer: "A1",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A1"],
+    public: false
+  },
+  {
+    id: 2,
+    user_id: 2,
+    question: "Q2",
+    url: "/",
+    answer: "A2",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A2"],
+    public: true
+  },
+  {
+    id: 3,
+    user_id: 3,
+    question: "Q3",
+    url: "/",
+    answer: "A3",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A3"],
+    public: false
+  },
+  {
+    id: 4,
+    user_id: 4,
+    question: "Q4",
+    url: "/",
+    answer: "A4",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A4"],
+    public: true
+  },
+  {
+    id: 5,
+    user_id: 5,
+    question: "Q5",
+    url: "/",
+    answer: "A5",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A5"],
+    public: false
+  },
+  {
+    id: 6,
+    user_id: 6,
+    question: "Q6",
+    url: "/",
+    answer: "A6",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A6"],
+    public: true
+  },
+  {
+    id: 7,
+    user_id: 7,
+    question: "Q7",
+    url: "/",
+    answer: "A7",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A7"],
+    public: false
+  },
+  {
+    id: 8,
+    user_id: 8,
+    question: "Q8",
+    url: "/",
+    answer: "A8",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A8"],
+    public: true
+  },
+  {
+    id: 9,
+    user_id: 9,
+    question: "Q9",
+    url: "/",
+    answer: "A9",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A9"],
+    public: false
+  },
+  {
+    id: 9,
+    user_id: 9,
+    question: "Q9",
+    url: "/",
+    answer: "A9",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A9"],
+    public: true
+  },
+  {
+    id: 10,
+    user_id: 10,
+    question: "Q10",
+    url: "/",
+    answer: "A10",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A10"],
+    public: false
+  },
+  {
+    id: 11,
+    user_id: 11,
+    question: "Q11",
+    url: "/",
+    answer: "A11",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A11"],
+    public: false
+  },
+  {
+    id: 12,
+    user_id: 12,
+    question: "Q12",
+    url: "/",
+    answer: "A12",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A12"],
+    public: false
+  },
+  {
+    id: 13,
+    user_id: 13,
+    question: "Q13",
+    url: "/",
+    answer: "A13",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A13"],
+    public: false
+  },
+  {
+    id: 14,
+    user_id: 14,
+    question: "Q14",
+    url: "/",
+    answer: "A14",
+    all_answers: ["decoy1", "decoy2", "decoy3", "A14"],
+    public: false
+  }
+]
