d3dc370ab2aa7e7d6327d760203f5b492fa866b5
Michael Chan <mikechansas@gmail.com>
2022-02-10 20:29:11 -0800

    Import isAuthenticated prop into Login Button Component via the useAuth0 Hook.

--- src/components/login-button/login-button.component.jsx
+++ src/components/login-button/login-button.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+    console.log(isAuthenticated);
 
     return (
-        (
+        !isAuthenticated && (
             <button onClick={() => loginWithRedirect()}>
                 Log In
             </button>
