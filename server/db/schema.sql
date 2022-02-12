588e422e926810603438261530d9cda7841ac5bf
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-02-12 11:36:30 -0500

    redo db schema and inserts for all tables except quizzes and related tables

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS decks;
+DROP TABLE IF EXISTS decks_with_cards;
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
@@ -9,7 +12,7 @@ CREATE TABLE users(
   pass VARCHAR(250)
 );
 
-CREATE TABLE users(
+CREATE TABLE cards(
   id SERIAL PRIMARY KEY,
   user_id REFERENCES users(id),
   question TEXT,
@@ -17,6 +20,25 @@ CREATE TABLE users(
   answer TEXT,
   all_answers TEXT[],
   public BOOLEAN
+);
+
+CREATE TABLE categories(
+  id SERIAL PRIMARY KEY,
+  category_name VARCHAR(250)
+);
+
+CREATE TABLE cards(
+  id SERIAL PRIMARY KEY,
+  user_id REFERENCES users(id),
+  name VARCHAR(250),
+  description TEXT,
+  category_id REFERENCES categories(id)
+);
+
+CREATE TABLE decks_with_cards(
+  id SERIAL PRIMARY KEY,
+  card_id REFERENCES cards(id),
+  deck_id REFERENCES decks(id)
 );
 
 INSERT INTO users (firstName, lastName, email, pass)
@@ -48,23 +70,23 @@ INSERT INTO cards (user_id, question, url, answer, all_answers, public)
 VALUES (1, "Q1","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A1", '{"F1", "F2", "F3", "A1"}', TRUE),
        (1, "Q2","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
-       "A2", '{"F1", "F2", "F3", "A2"}', TRUE),
+       "A2", '{"F1", "F2", "F3", "A2"}', FALSE),
        (1, "Q3","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A3", '{"F1", "F2", "F3", "A3"}', TRUE),
        (1, "Q4","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
-       "A4", '{"F1", "F2", "F3", "A4"}', TRUE),
+       "A4", '{"F1", "F2", "F3", "A4"}', FALSE),
        (1, "Q5","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A5", '{"F1", "F2", "F3", "A5"}', TRUE),
        (1, "Q6","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
-       "A6", '{"F1", "F2", "F3", "A6"}', TRUE),
+       "A6", '{"F1", "F2", "F3", "A6"}', FALSE),
        (1, "Q7","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A7", '{"F1", "F2", "F3", "A7"}', TRUE),
        (1, "Q8","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
-       "A8", '{"F1", "F2", "F3", "A8"}, TRUE)',
+       "A8", '{"F1", "F2", "F3", "A8"}', FALSE),
        (1, "Q9","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A9", '{"F1", "F2", "F3", "A9"}', TRUE),
        (1, "Q10","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
-       "A10", '{"F1", "F2", "F3", "A10"}', TRUE),
+       "A10", '{"F1", "F2", "F3", "A10"}', FALSE),
        (2, "Q11","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A11", '{"F1", "F2", "F3", "A11"}', FALSE),
        (2, "Q12","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
@@ -77,3 +99,20 @@ VALUES (1, "Q1","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQL
        "A15", '{"F1", "F2", "F3", "A15"}', FALSE),
        (3, "Q16","https://drive.google.com/file/d/1-zn90p7XF2bwQ_aJusE5NIUaajkRQLLo/view?usp=sharing",
        "A16", '{"F1", "F2", "F3", "A16"}', FALSE);
+
+INSERT INTO categories (category_name) VALUES ("SCIENCES"),
+                                              ("COMMUNICATION LANGUAGES"),
+                                              ("PROGRAMMING LANGUAGES"),
+                                              ("FRAMEWORKS"),
+                                              ("IDE"),
+                                              ("OTHER");
+
+INSERT INTO decks (user_id, name, description, category_id) VALUES 
+        (1,"First 10 cards", "the description of the deck with id = 1", 1),
+        (2,"First 5 Public cards and cards 11, 12, and 13", "the description of the deck with id = 2", 1),
+        (3,"First 5 Public cards and cards 14, 15, and 16", "the description of the deck with id = 3", 1);
+
+INSERT INTO decks_with_cards (card_id,deck_id) VALUES 
+        (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,1), (7,1), (9,1), (10,1)
+        (1,2), (3,2), (5,2), (7,2), (9,2),(11,2), (12,2), (13,2)
+        (1,3), (3,3), (5,3), (7,3), (9,3),(14,2), (15,2), (16,2);
