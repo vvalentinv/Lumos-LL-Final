467f042a22d5dac5ae0f77982aa417f2002e415f
Michael Chan <mikechansas@gmail.com>
2022-02-13 14:33:48 -0800

    Implement minor tidy up.

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -3,5 +3,10 @@ import { combineReducers } from 'redux';
 import cardListReducer from './card-list/card-list.reducer';
 
 export default combineReducers({
-    cardlist: cardListReducer
+    cardList: cardListReducer
 });
+
+
+//Home Page - useEffect componentdidMount all decks
+
+//Deck Preview - useEffect componentdidMount I'th Deck
