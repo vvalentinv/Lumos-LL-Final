41b1838e4ff246fe360c9b44b1ca90284036b56a
Suliman Fayyaz <hydrajin@gmail.com>
2022-02-14 19:55:01 -0500

    fixed axios issue for testing fetch after pulling from main

--- client/src/components/dataFetching.js/dataFetching.jsx
+++ client/src/components/dataFetching.js/dataFetching.jsx
@@ -8,13 +8,20 @@ const DataFetching = () => {
   // Use effect makes the request for you
   useEffect(() => {
     // get all cards from deck 1
-    axios.get(`/api/cards/3`).then((response) => {
+    axios.get(`/api/cards/1`)
+    .then((response) => {
       // console.log(response);
       console.log(response.data);
       // setCards(response.data);
       // use a callback to prevent infinite loop caused by a dependancy array
       setCards((prevCards) => {
         return [...prevCards, ...response.data];
+      })
+      //! Cannot read properties of undefined (reading 'catch')
+      .catch((error)=> {
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        console.log(error.response.data);
       });
     });
   }, []);
@@ -22,12 +29,16 @@ const DataFetching = () => {
   return (
     <div>
       <h2>Data Fetching!!!</h2>
+      <p>you have {cards.length} cards</p>
       {/* turn array of objects into an array of jsx elements  */}
       {cards.map((card) => {
         return (
-          <p key={card.id}>
-            Question: {card.question} | Answer: {card.answer} ({card.id})
-          </p>
+          <section key={card.id}>
+            <h1>Card {card.id}</h1>
+            <p>
+              Question: {card.question} | Answer: {card.answer} 
+            </p>
+          </section>
         );
       })}
     </div>
