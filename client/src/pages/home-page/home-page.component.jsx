6736d4e1baf93a26ae430945a2e3bfba1dedce4b
Michael Chan <mikechansas@gmail.com>
2022-02-14 18:11:12 -0800

    Input notes for future reference.

--- client/src/pages/home-page/home-page.component.jsx
+++ client/src/pages/home-page/home-page.component.jsx
@@ -1,6 +1,9 @@
 
 const HomePage = () => {
 
+    //useEffect => get All Decks for Current User via Axios => Store in useState Hook
+    //=> Iterate over useState Hook and display deck data
+
     return (
         <p>Home</p>
     );
