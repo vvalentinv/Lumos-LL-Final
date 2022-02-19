a4cd342cf8e17cd0c9bfa298ddd021d8edfc458b
Suliman Fayyaz <hydrajin@gmail.com>
2022-02-19 11:43:00 -0500

    modify deck preview page styling component and test MUI

--- client/src/components/preview-card/preview-card.component.jsx
+++ client/src/components/preview-card/preview-card.component.jsx
@@ -1,13 +1,57 @@
 
+import './preview-card.styles.scss'
+import Box from '@mui/material/Box';
+import { maxHeight } from "@mui/system";
+
 const PreviewCard = (props) => {
     const { term, definition } = props;
 
     return (
-        <>
+        <div className="q-a">
             
-            <p>{definition}</p>
-            <p>{term}</p>
-        </>
+            <div className="container-q">
+                <Box
+                    component="div"
+                    sx={{
+                    display: 'flow',
+                    p: 0.5,
+                    m: 0.5,
+                    bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#292f41' : '#292f41'),
+                    color: (theme) =>
+                    theme.palette.mode === 'dark' ? '#ffff' : '#ffff',
+                    border: '0.6px solid',
+                    borderColor: (theme) =>
+                        theme.palette.mode === 'dark' ? '#292f41' : '#292f41',
+                    borderRadius: 1.2,
+                    fontSize: '0.875rem',
+                    height: maxHeight,
+                    }}
+                >
+                    {definition}
+                </Box>
+            </div>
+            <div className="vertical"></div>
+            <div className="container-a">
+                <Box
+                    component="div"
+                    sx={{
+                    display: 'block',
+                    p: 0.5,
+                    m: 0.5,
+                    bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#292f41' : '#292f41'),
+                    color: (theme) =>
+                        theme.palette.mode === 'dark' ? '#ffff' : '#ffff',
+                    border: '0.6px solid',
+                    borderColor: (theme) =>
+                        theme.palette.mode === 'dark' ? '#292f41' : '#292f41',
+                    borderRadius: 1.2,
+                    fontSize: '0.875rem',
+                    }}
+                >
+                    {term}
+                </Box>
+            </div>
+        </div>
     )
 
 
