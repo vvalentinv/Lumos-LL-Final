63d694467db3938aa776f2b83e5844e0d5b5e77c
Michael Chan <mikechansas@gmail.com>
2022-02-19 19:24:25 -0800

    Integrate useNavigate hook into Home Page Card component.

--- client/src/components/home-page-card/home-page-card.jsx
+++ client/src/components/home-page-card/home-page-card.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 
+import './home-page-card.styles.scss';
+
 const HomePageCard = ({ deckName, deckID }) => {
     const navigate = useNavigate();
 
     return (
-        <div className='deck-name-container'>
-            <span onClick={() => navigate(`/deckpreview/${deckID}`)}>
+        <div className='deck-name-container' onClick={() => navigate(`/deckpreview/${deckID}`)}>
+            <span >
                 {deckName}
             </span>
         </div>
