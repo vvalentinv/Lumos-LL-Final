031213016d06302c0ff4c410621098482e7f02cd
Suliman Fayyaz <hydrajin@gmail.com>
2022-02-19 21:58:55 -0500

    update add-card-row hovering color change and style

--- client/src/components/add-card-row/add-card-row.component.jsx
+++ client/src/components/add-card-row/add-card-row.component.jsx
@@ -1,10 +1,16 @@
 
 import './add-card-row.styles.scss';
+import AddIcon from '@mui/icons-material/Add';
 
 const AddCardRow = ({ addCardHandler }) => {
     return (
         <div className='add-row-card-container' onClick={addCardHandler}>
-            <span className='add-card-row-text'>+ ADD CARD</span>
+            <div className='add-card-main'>
+                <div className='add-card-div'>
+                    <AddIcon fontSize='small'/>
+                    <span className='add-card-row-text'>ADD CARD</span>
+                </div>
+            </div>
         </div>
     )
 }
