8df6b245bdc0de77f28ffd2d8a38d1e5f1b381ca
Michael Chan <mikechansas@gmail.com>
2022-02-21 13:13:12 -0800

    Refactor logic of Home Page Card so that clicking trash can will summon Delete Pop Up Component.

--- client/src/components/home-page-card/home-page-card.jsx
+++ client/src/components/home-page-card/home-page-card.jsx
@@ -2,31 +2,30 @@ import { useNavigate } from 'react-router-dom';
 
 import './home-page-card.styles.scss';
 import DeleteIcon from '@mui/icons-material/Delete';
-import axios from 'axios';
 
 import { deleteDeck } from "../../helpers/selectors";
 
-const HomePageCard = ({ deckName, deckID, deckList, setDeckList }) => {
+const HomePageCard = ({ deckID, deckName, summonPopUp }) => {
 
     const navigate = useNavigate();
 
-    const deleteDeckFromDeckList = (deckID) => {
-        let updatedDeckList = deckList.filter((deck) => deck.id !== deckID);
-        setDeckList(updatedDeckList);
-        deleteDeck(deckID)
-            .then((result) => console.log("promise to delete deck resolved:", result))
-            .catch((error) => console.log(error))
-    }
+    // const deleteDeckFromDeckList = (deckID) => {
+    //     let updatedDeckList = deckList.filter((deck) => deck.id !== deckID);
+    //     setDeckList(updatedDeckList);
+    //     deleteDeck(deckID)
+    //         .then((result) => console.log("Deck Deleted:", result))
+    //         .catch((error) => console.log(error))
+    // }
 
     return (
-        <div className='deck-name-container' onClick={() => navigate(`/deckpreview/${deckID}`)}>
-            <span>
+        <div className='deck-name-container' >
+            <span onClick={() => navigate(`/deckpreview/${deckID}`)}>
                 {deckName}
             </span>
             <div className='delete-logo-container'>
                 <DeleteIcon
                     className='delete-logo'
-                    onClick={() => deleteDeckFromDeckList(deckID)}
+                    onClick={() => summonPopUp(deckID)}
                 />
             </div>
         </div>
