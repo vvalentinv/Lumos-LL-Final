beb60193b425514c4b2fbe8cbf10bd71ff305e11
Michael Chan <mikechansas@gmail.com>
2022-02-21 14:13:31 -0800

    Implement Home Page Skeleton Component.

---
+++ client/src/components/home-page-skeleton/home-page-skeleton.component.jsx
@@ -0,0 +1,7 @@
+const HomePageSkeleton = () => {
+    return (
+        <h2>There doesn't seem to be anything here. Why don't you create a new deck?</h2>
+    )
+}
+
+export default HomePageSkeleton;
