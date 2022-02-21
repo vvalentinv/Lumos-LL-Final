880db77a25ab616dc98d868a54c853256335c62f
Michael Chan <mikechansas@gmail.com>
2022-02-21 13:10:57 -0800

    Integrate Delete Pop Up Component into Home Page along with corresponding delete logic. Refactor Delete Icon to summon Delete Pop Up on click.

--- client/src/pages/home-page/home-page.component.jsx
+++ client/src/pages/home-page/home-page.component.jsx
@@ -4,14 +4,54 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { getDeckListForUser } from "../../helpers/selectors";
 
 import HomePageCard from "../../components/home-page-card/home-page-card";
+import DeletePopUp from "../../components/pop-up/pop-up.component";
+
+import { deleteDeck } from "../../helpers/selectors";
 
 const HomePage = () => {
 
   const { user } = useAuth0();
 
   const [deckList, setDeckList] = useState([]);
+  const [popup, setPopUp] = useState({
+    showPopUp: false,
+    deckID: null,
+  });
+
   const selUser = useSelector(state => state.user);
   const { userUUID } = selUser;
+
+  const summonPopUp = (deckID) => {
+    setPopUp({
+      showPopUp: true,
+      deckID: deckID
+    });
+  };
+
+  const handleDeleteTrue = () => {
+    if (popup.showPopUp && popup.deckID) {
+      let deleteDeckID = popup.deckID;
+
+      let updatedDeckList = deckList.filter((deck) => deck.id !== deleteDeckID);
+
+      setDeckList(updatedDeckList);
+
+      setPopUp({
+        showPopUp: false,
+        deckID: null,
+      });
+      deleteDeck(deleteDeckID)
+        .then((result) => console.log("Deck Deleted:", result))
+        .catch((error) => console.log(error))
+    }
+  };
+
+  const handleDeleteFalse = () => {
+    setPopUp({
+      showPopUp: false,
+      deckID: null,
+    });
+  };
 
   useEffect(() => {
     if (!userUUID) {
@@ -27,18 +67,26 @@ const HomePage = () => {
   return (
     <div className='deck-container'>
       <p>YOUR DECKS</p>
-      {deckList.length && deckList.map((deck) => {
+      {!deckList.length}
+      {deckList.map((deck) => {
         const { id, deck_name } = deck
         return (
           <HomePageCard
             key={id}
             deckID={id}
             deckName={deck_name}
-            deckList={deckList}
-            setDeckList={setDeckList}
+            summonPopUp={summonPopUp}
           />
         )
       })}
+      {popup.showPopUp && (
+        <DeletePopUp
+          deckList={deckList}
+          setDeckList={setDeckList}
+          handleDeleteTrue={handleDeleteTrue}
+          handleDeleteFalse={handleDeleteFalse}
+        />
+      )}
     </div>
   );
 }
