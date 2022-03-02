4a9ee183da40d776aa825a1fdae63cd92cbf830b
Michael Chan <mikechansas@gmail.com>
2022-03-01 16:41:38 -0800

    Implement clean up.

--- client/src/components/delete-pop-up/delete-pop-up.component.jsx
+++ client/src/components/delete-pop-up/delete-pop-up.component.jsx
@@ -1,18 +1,19 @@
 
-import './delete-pop-up.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
+
+import './delete-pop-up.styles.scss';
 
 const DeletePopUp = ({ handleDeleteTrue, handleDeleteFalse }) => {
     return (
         <div className='delete-pop-up-wrapper'>
             <div className='delete-pop-up-container'>
-                <h1 className='delete-warning'>Are you sure you want to delete this deck?</h1>
+                <h1 className='delete-pop-up-warning'>Are you sure you want to delete this deck?</h1>
                 <div className='delete-pop-up-buttons'>
-                    <CustomButton className='delete-Cancel' onClick={handleDeleteFalse}>
+                    <CustomButton className='pop-up-cancel-button' onClick={handleDeleteFalse}>
                         Cancel
                     </CustomButton>
-                    <div className='delete-Delete'>
+                    <div className='pop-up-delete-button'>
                         <CustomButton onClick={handleDeleteTrue}>
                             Confirm
                         </CustomButton>
