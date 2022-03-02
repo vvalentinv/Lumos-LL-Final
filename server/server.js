d928666109136be224e5a8bbf5a3d1a141e5911a
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-03-02 07:50:35 -0500

    remove commented out code

--- server/server.js
+++ server/server.js
@@ -3,15 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt');
-// const client = require('./db/connection');
-// const { getAllUsers } = require('./db/queries');
 
 //env variables
 const { PORT, ENVIRONMENT } = process.env;
-
-// console.log(process.env);
 
 //routes requires
 const userRoutes = require('./routes/users');
@@ -21,14 +15,10 @@ const categoriesRoutes = require('./routes/categories');
 
 //middleware
 const app = express();
-
 app.use(morgan(ENVIRONMENT));
-// new way of using middleware bodyParser
-// app.use(bodyParser.json());
-
 app.use(express.json());
-
 app.use(cors());
+
 //routes
 app.use('/api/users', userRoutes());
 app.use('/api/decks', deckRoutes());
